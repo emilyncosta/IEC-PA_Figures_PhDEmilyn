1c568b337c42594d3fb8c6ebf19685c88a3fa652
Abhinav Sharma <abhi18av@users.noreply.github.com>
2018-07-01 08:40:04 +0530

    Update pdf_to_png.rb

--- pdf_to_png.rb
+++ pdf_to_png.rb
@@ -4,6 +4,9 @@ files = Dir["*.pdf"]
 
 dpi_value = 500  # default value
 
+# with docker use the following command
+#  docker run -ti --rm -v /Users/eklavya/Desktop/figures:/pdf bwits/pdf2htmlex-alpine pdf2htmlEX --hdpi 500 --vdpi 500 --embed-image 0  hcc_eai.pdf
+
 pdf2htmlEX = "pdf2htmlEX --hdpi " + dpi_value.to_s + " --vdpi " + dpi_value.to_s + " --embed-image 0 "
 
 
